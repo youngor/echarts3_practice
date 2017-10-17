ff568e181d9726c8cea546c89a33c793458ca5a7
youngorchen <622000973@qq.com>
2017-10-17 17:44:04 +0800

    change filename

--- server/mini_web.rb
+++ server/mini_web.rb
@@ -7,26 +7,11 @@ require "base64"
          '----------'
   end
 
-=begin
-$picInfo = $_POST['baseimg'];
-
-　　$streamFileRand = date('YmdHis').rand(1000,9999); //图片名
-
-　　$picType ='.png';//图片后缀
-
-　　$streamFilename = "/www/echarts/".$streamFileRand .$picType; //图片保存地址
-
-　　preg_match('/(?<=base64,)[\S|\s]+/',$picInfo,$picInfoW);//处理base64文本
-
-　　file_put_contents($streamFilename,base64_decode($picInfoW[0]));//文件写入
-
-data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAlgAAAGQCAYAAAByNR6YAAAgAElEQVR4nO3dT2wkZ3rfcZ6TS98C33jMiVNTRSpaY5O04aFGzl9CpAADwYZNYbq0MWKbCZIcmD20RjHiTdZ50504XCW2mrazM0w4XQUkAI1NYJSGs80q2Ic2ViNE4KWB6JBjz8F35tBVVLGm+v+f532rvh/ggUZk/+EUNcOfnvep993YAAAAAAAAAAAAAAAAAAAUluM4p47jHEp/HQAAAAsJLKtytWOrVzsPe1c7DwdXOw9vl1T9l9tWY9T72rZ94jjO7ZR1Gj/nsW3blzmvdWnb9knmY1vJ83I+fjvmazpN/Tr367Fte2vGywwAAMrk5bbVWGKoe
-=end
-
   post '/' do
+    puts params.to_s
     b = /(?<=base64,)[\S|\s]+/.match(params['baseimg'])
-    puts   b  
-    File.open("myfile.png","wb") {|f|
+    #puts   b  
+    File.open(params['file_name'],"wb") {|f|
         f.write(Base64.decode64(b.to_s))  
     }
     'done!' #+ params.to_s
